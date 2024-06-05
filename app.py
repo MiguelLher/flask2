698b6cec5a2d10b37b339e45a0a4bc837446dd68
MiguelLher <milahe102@gmail.com>
2024-06-05 17:03:50 -0600

    Actualizacion app

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
-    return "Miguel Lara Hernandez - 9 A - 20200705"
+    return render_template('home.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    message = request.form.get('message')
+    return f"Nombre: {name}, Mensaje: {message}"
 
 if __name__ == '__main__':
     app.run(debug=True)
