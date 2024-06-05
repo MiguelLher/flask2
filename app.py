276bded1ed0e4189609b446f11bc41aea3215f87
MiguelLher <milahe102@gmail.com>
2024-06-05 17:58:56 -0600

    Actualización app

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    name = request.form.get('name')
-    message = request.form.get('message')
-    return f"Nombre: {name}, Mensaje: {message}"
 
 if __name__ == '__main__':
     app.run(debug=True)
