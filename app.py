375ab691a3557a1086e6e66ce586885896753d37
MiguelLher <milahe102@gmail.com>
2024-06-25 10:38:15 -0600

    videojuegos

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 # Cargar el modelo entrenado
-model = joblib.load('modeloRF.pkl')
+model = joblib.load('model.pkl')
 app.logger.debug('Modelo cargado correctamente.')
 
 @app.route('/')
@@ -20,28 +20,28 @@ def home():
 def predict():
     try:
         # Obtener los datos enviados en el request
-        infants = float(request.form['infants'])
-        satellite = float(request.form['satellite'])
-        missile = float(request.form['missile'])
-        education = float(request.form['education'])
-        superfund = float(request.form['superfund'])
-        crime = float(request.form['crime'])
+        app.logger.debug(f'Datos recibidos: {request.form}')
+        na_sales = float(request.form['na_sales'])
+        eu_sales = float(request.form['eu_sales'])
+        jp_sales = float(request.form['jp_sales'])
+        other_sales = float(request.form['other_sales'])
+        genre = float(request.form['genre'])
+        platform = float(request.form['platform'])
 
-        
         # Crear un DataFrame con los datos
-        data_df = pd.DataFrame([[infants, satellite,missile, education, superfund, crime ]], columns=['infants', 'satellite', 'missile', 'education', 'superfund','crime'])
+        data_df = pd.DataFrame([[na_sales, eu_sales, jp_sales, other_sales, genre, platform]], 
+                               columns=['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales', 'Genre', 'Platform'])
         app.logger.debug(f'DataFrame creado: {data_df}')
-        
+
         # Realizar predicciones
         prediction = model.predict(data_df)
         app.logger.debug(f'Predicción: {prediction[0]}')
-        
+
         # Devolver las predicciones como respuesta JSON
-        return jsonify({'categoria': prediction[0]})
+        return jsonify({'global_sales': prediction[0]})
     except Exception as e:
         app.logger.error(f'Error en la predicción: {str(e)}')
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
-
