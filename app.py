7606d9feca85725bc741d589892f475659bb96ca
MiguelLher <milahe102@gmail.com>
2024-06-25 11:11:22 -0600

    videojuegos

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 # Cargar el modelo entrenado
-model = joblib.load('modeloo.pkl')
+model = joblib.load('model.pkl')
 app.logger.debug('Modelo cargado correctamente.')
 
 @app.route('/')
